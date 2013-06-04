b0438335a655a42f4ece9cae8efe67bfc4694e58
Alejandro Castro <alejandrocastrounqui@gmail.com>
2013-06-04 00:48:29 -0300

    travis database configuration

--- config/database.rb
+++ config/database.rb
@@ -16,5 +16,6 @@ case Padrino.env
   when :development then DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "log_de_tareas_development.db"))
   when :production  then DataMapper.setup(:default, ENV['DATABASE_URL'])
   when :test        then DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "log_de_tareas_test.db"))
- 	when :travis      then DataMapper.setup(:default, "mysql://root@localhost/log_de_tareas_test.db")
+  when :travis      then DataMapper.setup(:default, "mysql://root@localhost/log_de_tareas_test")
+  when :staging     then DataMapper.setup(:default, ENV['DATABASE_URL'])
 end
