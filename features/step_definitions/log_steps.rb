0b25c05b4d10ea1cd3e30a72e94fe53de6359eaf
linaresmariano <marianolinares92@gmail.com>
2013-06-23 05:54:49 -0300

    steps

--- features/step_definitions/log_steps.rb
+++ features/step_definitions/log_steps.rb
@@ -4,8 +4,17 @@ Given(/^I have assigned the task with name "(.*?)" and time estimated for the gr
 end
 
 Given(/^I have assigned the task with name "(.*?)" of the group "(.*?)"$/) do |task_name, group|
-  step %{there is task with name "#{task_name}" for the group "#{group}"}
-  BuilderTask.new().build().whit_task_template(TaskTemplate.find_by_name(task_name))
+  #step %{there is task with name "#{task_name}" for the group "#{group}"}
+  #BuilderTask.new().build().whit_task_template(TaskTemplate.find_by_name(task_name))
+  
+  #group = Group.find_by_name(group_name)
+  #TaskTemplate.create(task_name, 'a_fake_description', group)
+  account = Account.find_by_name('a_build_name')
+  grupo = Group.find_by_name(group)
+  task_template = TaskTemplate.create(task_name, "description", grupo)
+  
+  task = Task.create(task_template, account)
+  
 end
 
 
